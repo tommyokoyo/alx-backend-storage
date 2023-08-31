754ae9fb44ef79cbd6a3cb4d44fd0a00630a35ec
tommyokoyo <okoyotommy@gmail.com>
2023-08-31 12:37:09 +0300

    We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --Script that creates TABLE Users--
-
-CREATE TABLE IF NOT EXISTS `users`(
-    `id` SERIAL NOT NULL PRIMARY KEY,
+-- script can be executed on any database
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `email` VARCHAR(255) NOT NULL UNIQUE,
     `name` VARCHAR(255)
 );
