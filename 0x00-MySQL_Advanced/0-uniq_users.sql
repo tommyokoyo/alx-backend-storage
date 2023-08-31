7e26ce787480aef8f5981767dd19fe7bac6eef58
tommyokoyo <okoyotommy@gmail.com>
2023-08-31 12:47:43 +0300

    Old school band

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 --Script that creates TABLE Users--
 -- script can be executed on any database
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `users`
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `email` VARCHAR(255) NOT NULL UNIQUE,
-    `name` VARCHAR(255)
-);
+    `name` VARCHAR(255);
